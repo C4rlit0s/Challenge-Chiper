5ad2d280a3389cec25544355e25c7553ff029ee4
Carlos Abreu <parakletos75@gmail.com>
2021-11-22 16:24:24 -0400

    Challenge complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { QueryClient, QueryClientProvider } from "react-query";
+import PrimarySearchAppBar from "../src/components/Header";
+import SimpleContainer from "../src/containers/ListBikes";
+import "./App.css";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <QueryClientProvider client={queryClient}>
+        <PrimarySearchAppBar />
+        <SimpleContainer />
+      </QueryClientProvider>
     </div>
   );
 }
